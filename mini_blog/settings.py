7a1c067f15417cb5ae5be641b5ee05af5c20631d
adhendo <adamdh93@gmail.com>
2020-02-29 16:10:26 -0800

    env support

--- mini_blog/settings.py
+++ mini_blog/settings.py
@@ -12,11 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-import environ
 
-env = environ.Env()
-
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -28,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
