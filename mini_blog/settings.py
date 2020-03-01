c54a7e26420df8e493e4e250a326b69e4737fc59
adhendo <adamdh93@gmail.com>
2020-02-29 18:12:28 -0800

    gunicorn

--- mini_blog/settings.py
+++ mini_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'https://fathomless-everglades-09479.herokuapp.com/']
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 # Application definition
